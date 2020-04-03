8d42dec3b942216849c6d32bf2a452dab699ddf0
Eliott <eliott.vanderstraten@mic-belgique.be>
2020-04-03 21:39:49 +0200

    Add map in mobile application

--- FrontEnd/walkingdead/App.js
+++ FrontEnd/walkingdead/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
+
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Walking dead</Text>
+//     </View>
+//   );
+// }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Walking dead</Text>
-    </View>
+    <MapView style={{ flex: 1 }} region={{ latitude: 42.882004, longitude: 74.582748, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }} showsUserLocation={true} />
   );
 }
 
